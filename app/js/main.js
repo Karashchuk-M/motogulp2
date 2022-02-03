675386a3cd49fcb5d43bdb8931dce986310de6c3
Karashchuk_M <myranika@gmail.com>
2022-02-03 13:26:24 +0200

    <сообщение_коммита>

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+$('.top__slider').slick({
+    dots: false,
+    arrows: false,
+    slidesToScroll: 0.5,
+    slidesToShow: 2.5,
+    autoplay: true,
+});
+
+$('.mission__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+});
+$('.whywe__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToScroll: 0.5,
+    slidesToShow: 1.6,
+});
+$('.whatinterestsyou__slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+});
+		
+
+
+})
